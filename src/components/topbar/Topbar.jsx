42e348e211296aff6e8fcf4c8ce3791fc0120500
DragosDev <dragosmotrea0@gmail.com>
2022-05-23 11:20:32 +0300

    Created the Material UI Icons for the Notifications Language Settings

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './topbar.css';
-import {NotificationsNone} from '@material-ui/icons';
+import {NotificationsNone, Language, Settings } from '@material-ui/icons';
 
 export default function Topbar() {
   return (
@@ -12,10 +12,20 @@ export default function Topbar() {
             </div>
 
             <div className="topRight">
+
                 <div className="topbarIconContainer">
                   <NotificationsNone/>
                 <span className="topIconBadge">2</span>
                 </div>
+
+                <div className="topbarIconContainer">
+                  <Language/>
+                </div>
+
+                <div className="topbarIconContainer">
+                  <Settings/>
+                </div>
+                <img src="https://images.unsplash.com/photo-1618641986557-1ecd230959aa?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387" alt="" className="topProfile" />
             </div>
 
         </div>
