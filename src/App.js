2aa5d94d04a30c265a4b99dfd29692245a5f48c7
DragosDev <dragosmotrea0@gmail.com>
2022-05-23 13:21:36 +0300

    Importing App.css files Defining Sidebar component inside of a div container
     <div className="container">
          <Sidebar/>
          </div>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Topbar from './components/topbar/Topbar'
+import Sidebar from './components/sidebar/Sidebar'
+import './App.css'
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <div className="container">
+      <Sidebar/>
+      </div>
+      
     </div>
   )
 }
